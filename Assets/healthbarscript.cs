140757fe6d70a5fbd4a50e900a9e8098d6228e2f
Tristan <talaliberte@yahoo.com>
2020-11-13 05:50:18 -0600

    Progress
    AHHHHH EVERYTHING IS PRETTY MUCH DONE!
    The only problem is that walk forward doesn't work, despite me doing the same thing as everything else, but the core mechanics work otherwise!
    Am going to record, write the final document, then say hasta la vista to this project.

---
+++ Assets/healthbarscript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class healthbarscript : MonoBehaviour
+{
+
+    public Slider slider;
+
+    [SerializeField] GameObject plusTwenty;
+
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+    }
+    
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+    }
+
+    private void Awake()
+    {
+        SetHealth(80);
+        SetMaxHealth(100);
+    }
+
+    private void Update()
+    {
+        ExampleCourotine();
+    }
+
+    IEnumerator ExampleCourotine()
+    {
+        yield return new WaitForSeconds(.2f);
+
+        plusTwenty.SetActive(false);
+    }
+}
